745a863ef7e260e69a21d3a37b9546f6fdc61836
Some1 <111959143+s1i1@users.noreply.github.com>
2022-09-12 16:27:51 +0300

    Добавил cards и search

--- src/App.js
+++ src/App.js
@@ -26,6 +26,117 @@ function App() {
           </div>
         </div>
       </header>
+      <main>
+        <div className="content">
+        <div className="content__top">
+          <h1 className="content__title">Все кроссовки</h1>
+          <div className="content__search search">
+            <div className="search__icon">
+            <img width={'14px'} height={'14px'} src="/img/main/search/search.svg" alt="search" />
+            </div>
+          <input placeholder="Поиск..."/>
+           </div>
+          </div>
+          <div className="cards">
+            <div className="cards__item item">
+              <button className="item__liked">
+                <img src="img/main/cards/not_liked.png" alt="unlike" />
+              </button>
+              <div className="item__product">
+                <img width={'133px'} height={'112px'} src="/img/main/cards/product1.jpg" alt="product" />
+              </div>
+              <p className="item__desc">
+                Мужские Кроссовки Nike Blazer Mid Suede
+              </p>
+              <div className="item__price price">
+              <div className="price__block">
+              <p className="price__title">
+                  Цена:
+                </p>
+                <strong className="price__value">
+                  12 999 руб.
+                </strong>
+              </div>
+              <button className="price__check">
+                <img src="img/main/cards/not_checked.svg" alt="not_checked" />
+              </button>
+              </div>
+            </div>
+            <div className="cards__item item">
+              <button className="item__liked">
+                <img src="img/main/cards/not_liked.png" alt="unlike" />
+              </button>
+              <div className="item__product">
+                <img width={'133px'} height={'112px'} src="/img/main/cards/product1.jpg" alt="product" />
+              </div>
+              <p className="item__desc">
+                Мужские Кроссовки Nike Blazer Mid Suede
+              </p>
+              <div className="item__price price">
+              <div className="price__block">
+              <p className="price__title">
+                  Цена:
+                </p>
+                <strong className="price__value">
+                  12 999 руб.
+                </strong>
+              </div>
+              <button className="price__check">
+                <img src="img/main/cards/not_checked.svg" alt="not_checked" />
+              </button>
+              </div>
+            </div>
+            <div className="cards__item item">
+              <button className="item__liked">
+                <img src="img/main/cards/not_liked.png" alt="unlike" />
+              </button>
+              <div className="item__product">
+                <img width={'133px'} height={'112px'} src="/img/main/cards/product1.jpg" alt="product" />
+              </div>
+              <p className="item__desc">
+                Мужские Кроссовки Nike Blazer Mid Suede
+              </p>
+              <div className="item__price price">
+              <div className="price__block">
+              <p className="price__title">
+                  Цена:
+                </p>
+                <strong className="price__value">
+                  12 999 руб.
+                </strong>
+              </div>
+              <button className="price__check">
+                <img src="img/main/cards/not_checked.svg" alt="not_checked" />
+              </button>
+              </div>
+            </div>
+            <div className="cards__item item">
+              <button className="item__liked">
+                <img src="img/main/cards/not_liked.png" alt="unlike" />
+              </button>
+              <div className="item__product">
+                <img width={'133px'} height={'112px'} src="/img/main/cards/product1.jpg" alt="product" />
+              </div>
+              <p className="item__desc">
+                Мужские Кроссовки Nike Blazer Mid Suede
+              </p>
+              <div className="item__price price">
+              <div className="price__block">
+              <p className="price__title">
+                  Цена:
+                </p>
+                <strong className="price__value">
+                  12 999 руб.
+                </strong>
+              </div>
+              <button className="price__check">
+                <img src="img/main/cards/not_checked.svg" alt="not_checked" />
+              </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
