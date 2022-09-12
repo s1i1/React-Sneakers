d68502af8d7d13e5e5a9eca2c99668895b663433
Some1 <111959143+s1i1@users.noreply.github.com>
2022-09-12 14:06:50 +0300

    Добавил Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
+      <header>
+        <div className="header-block">
+          <div className="logo-block">
+            <img width={'40px'} height={'40px'} src="img/header/logo.png" alr="logo" />
+            <div>
+              <h3>REACT SNEAKERS</h3>
+              <p>Магазин лучших кроссовок</p>
+            </div>
+          </div>
+          <div className="profile-block">
+            <ul>
+              <li className="basket">
+                <img width={'18px'} height={'18px'} src="/img/header/basket_icon.svg" alr="basket_icon" />
+                <p>1205 руб.</p>
+              </li>
+              <li>
+                <img width={'12px'} height={'19px'} src="/img/header/like_icon.svg" alr="like_icon" />
+              </li>
+              <li>
+                <img width={'20px'} height={'20px'} src="/img/header/profile_icon.svg" alr="profile_icon" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
