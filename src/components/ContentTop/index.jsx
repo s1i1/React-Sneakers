e3821bfbe7b079043ef77e41343bcbc3f536eab5
Some1 <111959143+s1i1@users.noreply.github.com>
2022-09-14 16:36:34 +0300

    Разбивка стилей ContentTop на модули

--- src/components/ContentTop/index.jsx
+++ src/components/ContentTop/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import styles from './ContentTop.module.scss';
 
 const ContentTop = () => {
     return (
-        <div className="content__top">
-            <h1 className="content__title">Все кроссовки</h1>
-            <div className="content__search search">
-                <div className="search__icon">
+        <div className={styles.top}>
+            <h1>Все кроссовки</h1>
+            <div className={styles.search}>
+                <div className={styles.search__icon}>
                     <img width={'14px'} height={'14px'} src="/img/main/search/search.svg" alt="search" />
                 </div>
                 <input placeholder="Поиск..." />
