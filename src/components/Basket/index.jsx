0b473334e09a10889c7a68b135990610d89e56e8
Some1 <111959143+s1i1@users.noreply.github.com>
2022-09-17 16:08:24 +0300

    Добавил крестик в basket

--- src/components/Basket/index.jsx
+++ src/components/Basket/index.jsx
@@ -11,7 +11,13 @@ const Basket = ({ onClickOverlay, items = [] }) => {
                 onClick={onClickOverlay}
             ></div>
             <div className={styles.basket__container}>
-                <h3 className={styles.basket__title}>Корзина</h3>
+                <div className={styles.basket__title}>
+                    <h3 className={styles.basket__title_text}>Корзина</h3>
+                    <button className={styles.basket__title_button}>
+                        <img width={'25px'} height={'25px'}
+                            src="/img/main/basket/delete_button.svg" alt="delete_button" />
+                    </button>
+                </div>
                 <div className={styles.basket__cards}>
                     {items.map((item, index) => {
                         return (
