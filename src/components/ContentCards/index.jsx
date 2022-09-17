d4ca3263891d0e7a06b6d0b986d4fd3c4b479e04
Some1 <111959143+s1i1@users.noreply.github.com>
2022-09-17 18:23:24 +0300

    Деструктуризация props

--- src/components/ContentCards/index.jsx
+++ src/components/ContentCards/index.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Card from './Card';
 import styles from "./ContentCards.module.scss";
 
-const ContentCards = (props) => {
+const ContentCards = ({ productData, onPlus }) => {
     return (
         <div className={styles.cards}>
-            {props.productData.map((item, index) => {
+            {productData.map((item, index) => {
                 return (
                     <Card key={index}
                         product={item.product}
                         desc={item.desc}
                         price={item.price}
-                        onPlus={props.onPlus}
+                        onPlus={onPlus}
                     />);
             })}
         </div>
