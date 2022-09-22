5e669864ee53915ea071572594e7ee8ad6ed3654
Some1 <111959143+s1i1@users.noreply.github.com>
2022-09-22 16:23:40 +0300

    Сверстал BasketEmpty

--- src/components/Basket/BasketEmpty.jsx
+++ src/components/Basket/BasketEmpty.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styles from './Basket.module.scss';
 
 const BasketEmpty = () => {
     return (
-        <div>BasketEmpty</div>
+        <div className={styles.empty}>
+            <div className={styles.empty__image}>
+                <img width={'120px'} height={'120px'} src='./img/main/basket/box.png' alt='box' />
+            </div>
+            <div className={styles.empty__header}>
+                <h2 className={styles.empty__header_title}>
+                    Корзина пустая
+                </h2>
+                <p className={styles.empty__header_subtitle}>
+                    Добавьте хотя бы одну пару
+                    <br />кроссовок, чтобы сделать заказ.
+                </p>
+            </div>
+            <button className={styles.empty__button}>
+                <div>
+                    <img width={'14px'} height={'12px'} src='./img/main/basket/arrow-left.svg' alt='arrow' />
+                    <p>Вернуться назад</p>
+                </div>
+            </button>
+        </div>
     )
 }
 
