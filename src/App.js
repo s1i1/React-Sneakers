116683300a7ec4ea23c28bdc5b45c332814c71c1
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-29 17:21:35 +0300

    Поменял бэк

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import AppContext from "./AppContext";
-import axios from "axios";
+import AppContext from './AppContext';
+import axios from 'axios';
 import HomePage from './pages/HomePage';
 import FavoritePage from './pages/FavoritePage';
 import PurchasesPage from './pages/PurchasesPage';
-import Header from "./components/Header";
-import Basket from "./components/Basket";
-import setOverflow from "./setOverflow";
-import calculateTotal from "./components/Basket/calculateTotal";
+import Header from './components/Header';
+import Basket from './components/Basket';
+import setOverflow from './setOverflow';
+import calculateTotal from './components/Basket/calculateTotal';
 
 function App() {
   const [productData, setProductData] = React.useState([]);
@@ -31,35 +31,37 @@ function App() {
       try {
         setIsLoading(true);
 
-        let resp = await axios.get("https://63234cd3362b0d4e7de0f3ee.mockapi.io/items");
+        let resp = await axios.get('https://6385f366875ca3273d4b3960.mockapi.io/items');
         setProductData(resp.data);
         setSearchItems(resp.data);
 
         setIsLoading(false);
       } catch (err) {
-        alert('Не удалось загрузить items')
+        alert('Не удалось загрузить items');
       }
     }
 
     async function getFavoriteData() {
       try {
-        let resp = await axios.get("https://63234cd3362b0d4e7de0f3ee.mockapi.io/favorite");
+        let resp = await axios.get('https://6385f366875ca3273d4b3960.mockapi.io/favorite');
 
         setFavoriteData(resp.data);
         setFavoriteItems(resp.data);
       } catch (err) {
-        alert('Не удалось загрузить favorite')
+        alert('Не удалось загрузить favorite');
       }
     }
 
     getData();
     getFavoriteData();
 
-    axios.get("https://63234cd3362b0d4e7de0f3ee.mockapi.io/basket")
-      .then(resp => setCardItems(resp.data));
+    axios
+      .get('https://6385f366875ca3273d4b3960.mockapi.io/basket')
+      .then((resp) => setCardItems(resp.data));
 
-    axios.get("https://63234cd3362b0d4e7de0f3ee.mockapi.io/purchases")
-      .then(resp => setPurchasesHistory(resp.data));
+    axios
+      .get('https://6385f366875ca3273d4b3960.mockapi.io/purchases')
+      .then((resp) => setPurchasesHistory(resp.data));
   }, []);
 
   React.useEffect(() => {
@@ -67,18 +69,18 @@ function App() {
   }, [cardItems]);
 
   React.useEffect(() => {
-    let jsonData = new Set(purchasesData.map(item => JSON.stringify(item)));
-    let arrData = Array.from(jsonData).map(item => JSON.parse(item));
+    let jsonData = new Set(purchasesData.map((item) => JSON.stringify(item)));
+    let arrData = Array.from(jsonData).map((item) => JSON.parse(item));
 
     if (arrData.length > 0) {
       setPurchasesHistory(arrData);
 
-      arrData.forEach(item => {
+      arrData.forEach((item) => {
         try {
           console.log('Отправляется obj в purchases');
-          axios.post("https://63234cd3362b0d4e7de0f3ee.mockapi.io/purchases", item);
+          axios.post('https://6385f366875ca3273d4b3960.mockapi.io/purchases', item);
         } catch (err) {
-          alert('purchases не отправлен')
+          alert('purchases не отправлен');
         }
       });
     }
@@ -90,36 +92,38 @@ function App() {
 
   const handlerClickDeleteCard = async (path) => {
     try {
-      let { data } = await axios.get("https://63234cd3362b0d4e7de0f3ee.mockapi.io/basket");
+      let { data } = await axios.get('https://6385f366875ca3273d4b3960.mockapi.io/basket');
 
       data.map((item, index) => {
         if (item.product.includes(path)) {
-          axios.delete(`https://63234cd3362b0d4e7de0f3ee.mockapi.io/basket/${item.id}`);
+          axios.delete(`https://6385f366875ca3273d4b3960.mockapi.io/basket/${item.id}`);
           setCardItems(cardItems.filter((arr, i) => cardItems[i] !== cardItems[index]));
         }
       });
     } catch (err) {
-      alert('Не удалось получить basket')
+      alert('Не удалось получить basket');
     }
-  }
+  };
 
   const handlerClickDeleteFavorite = async (path) => {
     try {
-      let { data } = await axios.get("https://63234cd3362b0d4e7de0f3ee.mockapi.io/favorite");
+      let { data } = await axios.get('https://6385f366875ca3273d4b3960.mockapi.io/favorite');
 
       data.map((item, index) => {
         if (item.product.includes(path)) {
-          axios.delete(`https://63234cd3362b0d4e7de0f3ee.mockapi.io/favorite/${item.id}`);
+          axios.delete(`https://6385f366875ca3273d4b3960.mockapi.io/favorite/${item.id}`);
 
-          setFavoriteItems(favoriteItems.filter((arr, i) => favoriteItems[i] !== favoriteItems[index]));
+          setFavoriteItems(
+            favoriteItems.filter((arr, i) => favoriteItems[i] !== favoriteItems[index]),
+          );
 
           setFavoriteData(favoriteData.filter((arr, i) => favoriteData[i] !== favoriteData[index]));
         }
       });
     } catch (err) {
-      alert('Не удалось получить favorite')
+      alert('Не удалось получить favorite');
     }
-  }
+  };
 
   const handlerClickSearchDelete = () => {
     setSearchValue('');
@@ -129,9 +133,9 @@ function App() {
   const onAddToCart = (obj) => {
     try {
       setCardItems((prev) => [...prev, obj]);
-      axios.post("https://63234cd3362b0d4e7de0f3ee.mockapi.io/basket", obj);
+      axios.post('https://6385f366875ca3273d4b3960.mockapi.io/basket', obj);
     } catch (err) {
-      alert('Не отправить obj в basket')
+      alert('Не отправить obj в basket');
     }
   };
 
@@ -139,7 +143,7 @@ function App() {
     try {
       setFavoriteItems((prev) => [...prev, obj]);
       setFavoriteData((prev) => [...prev, obj]);
-      axios.post("https://63234cd3362b0d4e7de0f3ee.mockapi.io/favorite", obj);
+      axios.post('https://6385f366875ca3273d4b3960.mockapi.io/favorite', obj);
     } catch (err) {
       alert('Не удалось отправить obj в favorite');
     }
@@ -151,59 +155,71 @@ function App() {
 
   const onChangeSearch = (e) => {
     setSearchValue(e.target.value);
-    setSearchItems(productData.filter(item => item.desc.toLowerCase().includes(e.target.value.toLowerCase())));
+    setSearchItems(
+      productData.filter((item) => item.desc.toLowerCase().includes(e.target.value.toLowerCase())),
+    );
   };
 
   return (
     <AppContext.Provider value={{ purchasesHistory, cardItems, total, setTotal, tax, setTax }}>
       <div className="wrapper">
-        {visibleBasket && <Basket
-          onClickCloseOverlay={handlerBasketClick}
-          onClickDeleteCard={handlerClickDeleteCard}
-          onPurchase={(obj) => onPurchase(obj)}
-        />}
-        <header>
-          <Header
-            onClickBasket={handlerBasketClick}
+        {visibleBasket && (
+          <Basket
+            onClickCloseOverlay={handlerBasketClick}
+            onClickDeleteCard={handlerClickDeleteCard}
+            onPurchase={(obj) => onPurchase(obj)}
           />
+        )}
+        <header>
+          <Header onClickBasket={handlerBasketClick} />
         </header>
         <main>
           <div className="content">
             <Routes>
-              <Route path="/" element={
-                <HomePage
-                  handlerClickSearchDelete={handlerClickSearchDelete}
-                  onChangeSearch={onChangeSearch}
-                  searchValue={searchValue}
-                  cardItems={cardItems}
-                  productData={searchItems}
-                  favoriteItems={favoriteItems}
-                  onPlus={(obj) => onAddToCart(obj)}
-                  onDelete={handlerClickDeleteCard}
-                  onDeleteFavorite={handlerClickDeleteFavorite}
-                  onFavorite={(obj) => onAddToFavorite(obj)}
-                  isLoading={isLoading}
-                />
-              } />
-              <Route path="/favorite" element={
-                <FavoritePage
-                  cardItems={cardItems}
-                  favoriteItems={favoriteItems}
-                  productData={favoriteData}
-                  onPlus={(obj) => onAddToCart(obj)}
-                  onDelete={handlerClickDeleteCard}
-                  onDeleteFavorite={handlerClickDeleteFavorite}
-                  onFavorite={(obj) => onAddToFavorite(obj)}
-                />
-              } />
-              <Route path="/purchases" element={
-                <PurchasesPage
-                  favoriteItems={favoriteItems}
-                  onPlus={(obj) => onAddToCart(obj)}
-                  onDelete={handlerClickDeleteCard}
-                  onDeleteFavorite={handlerClickDeleteFavorite}
-                  onFavorite={(obj) => onAddToFavorite(obj)}
-                />} />
+              <Route
+                path="/"
+                element={
+                  <HomePage
+                    handlerClickSearchDelete={handlerClickSearchDelete}
+                    onChangeSearch={onChangeSearch}
+                    searchValue={searchValue}
+                    cardItems={cardItems}
+                    productData={searchItems}
+                    favoriteItems={favoriteItems}
+                    onPlus={(obj) => onAddToCart(obj)}
+                    onDelete={handlerClickDeleteCard}
+                    onDeleteFavorite={handlerClickDeleteFavorite}
+                    onFavorite={(obj) => onAddToFavorite(obj)}
+                    isLoading={isLoading}
+                  />
+                }
+              />
+              <Route
+                path="/favorite"
+                element={
+                  <FavoritePage
+                    cardItems={cardItems}
+                    favoriteItems={favoriteItems}
+                    productData={favoriteData}
+                    onPlus={(obj) => onAddToCart(obj)}
+                    onDelete={handlerClickDeleteCard}
+                    onDeleteFavorite={handlerClickDeleteFavorite}
+                    onFavorite={(obj) => onAddToFavorite(obj)}
+                  />
+                }
+              />
+              <Route
+                path="/purchases"
+                element={
+                  <PurchasesPage
+                    favoriteItems={favoriteItems}
+                    onPlus={(obj) => onAddToCart(obj)}
+                    onDelete={handlerClickDeleteCard}
+                    onDeleteFavorite={handlerClickDeleteFavorite}
+                    onFavorite={(obj) => onAddToFavorite(obj)}
+                  />
+                }
+              />
             </Routes>
           </div>
         </main>
